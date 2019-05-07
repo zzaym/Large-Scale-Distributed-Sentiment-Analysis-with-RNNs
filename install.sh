5b574899925650dcb6ccba586acd02f83e26bd5a
Rong Liu <rongliu@g.harvard.edu>
2019-05-07 10:57:16 -0400

    updated bootstrap file

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo easy_install pip
-sudo /usr/local/bin/pip install boto3 nltk scikit-learn pandas
---
+++ install_boto3_h5py.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo easy_install pip
+sudo /usr/local/bin/pip install --upgrade pip
+sudo /usr/local/bin/pip install boto3
+sudo /usr/local/bin/pip install h5py
