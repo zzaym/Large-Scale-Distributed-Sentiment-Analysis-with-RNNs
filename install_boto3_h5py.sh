8536ab74d865f272c3765025fe6f96cb8562afa4
sophieyanzhao <33760176+sophieyanzhao@users.noreply.github.com>
2019-05-07 22:24:56 -0400

    Update install_boto3_h5py.sh

--- install_boto3_h5py.sh
+++ install_boto3_h5py.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+'''
+This file is used to perform bootstrap action in AWS EMR and install software.
+'''
 sudo easy_install pip
 sudo /usr/local/bin/pip install --upgrade pip
 sudo /usr/local/bin/pip install boto3
