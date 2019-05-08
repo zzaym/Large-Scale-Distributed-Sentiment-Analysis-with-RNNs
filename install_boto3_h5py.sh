3e57dd33956ccd1e0da670e86ef0c1911286762e
Yunbin Zhang <mzhangyb@gmail.com>
2019-05-08 00:38:03 -0400

    restructure files

--- install_boto3_h5py.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-'''
-This file is used to perform bootstrap action in AWS EMR and install software.
-'''
-sudo easy_install pip
-sudo /usr/local/bin/pip install --upgrade pip
-sudo /usr/local/bin/pip install boto3
-sudo /usr/local/bin/pip install h5py
